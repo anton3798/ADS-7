a916c89d1a7b896f4a42db8b85cf8339c6e10b9b
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-06-02 00:57:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,28 @@
 #include "train.h"
 
 int main() {
-  std::srand(static_cast<unsigned>(std::time(nullptr)));
-  const int N = 60;
-
-  Train trainOn;
-  for (int i = 0; i < N; ++i)
-    trainOn.addCar(true);
-  std::cout << trainOn.getLength()
-          << " " << trainOn.getOpCount() << std::endl;
-
-  Train trainOff;
-  for (int i = 0; i < N; ++i)
-    trainOff.addCar(false);
-  std::cout << trainOff.getLength()
-          << " " << trainOff.getOpCount() << std::endl;
-
-  Train trainRand;
-  for (int i = 0; i < N; ++i)
-    trainRand.addCar(std::rand() % 2 == 1);
-  std::cout << trainRand.getLength()
-          << " " << trainRand.getOpCount() << std::endl;
-  return 0;
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    for (int n = 2; n <= 30; n += 2) {
+        Train t_off;
+        for (int i = 0; i < n; ++i)
+            t_off.addCar(false);
+        t_off.getLength();
+        int off_steps = t_off.getOpCount();
+        Train t_on;
+        for (int i = 0; i < n; ++i)
+            t_on.addCar(true);
+        t_on.getLength();
+        int on_steps = t_on.getOpCount();
+        int sum_rand = 0;
+        for (int trial = 0; trial < 5; ++trial) {
+            Train t_rand;
+            for (int i = 0; i < n; ++i)
+                t_rand.addCar(std::rand() % 2 == 1);
+            t_rand.getLength();
+            sum_rand += t_rand.getOpCount();
+        }
+        int avg_rand = sum_rand / 5;
+        std::cout << n << "\t" << off_steps << "\t" << on_steps << "\t" << avg_rand << std::endl;
+    }
+    return 0;
 }
